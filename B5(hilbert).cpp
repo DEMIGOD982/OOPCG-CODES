8f65ee11794cc027638f73905999f6af6f6d9ae3
DEMIGOD982 <121516216+DEMIGOD982@users.noreply.github.com>
2022-12-28 00:51:56 +0530

    Create B5(hilbert).cpp

---
+++ B5(hilbert).cpp
@@ -0,0 +1,64 @@
+/*Write C++ program to generate Hilbert curve using concept of fractals (use
+constructor). */
+
+#include <iostream>
+#include <stdlib.h>
+#include <graphics.h>
+#include <math.h>
+using namespace std;
+
+void move(int j,int h,int &x,int &y)
+{
+if(j==2)
+x+=h;
+else if(j==3)
+y+=h;
+else if(j==4)
+x-=h;
+else if(j==1)
+y-=h;
+lineto(x,y);
+
+
+/*if(j==1)
+y-=h;
+else if(j==2)
+x+=h;
+else if(j==3)
+y+=h;
+else if(j==4)
+x-=h;
+lineto(x,y);*/
+}
+
+void hilbert(int r,int d,int l,int u,int i,int h,int &x,int &y)
+{
+if(i>0)
+{
+i--;
+hilbert(d,r,u,l,i,h,x,y);  //right
+move(r,h,x,y);
+hilbert(r,d,l,u,i,h,x,y);  //down
+move(d,h,x,y);
+hilbert(r,d,l,u,i,h,x,y);  //left
+move(l,h,x,y);
+hilbert(u,l,d,r,i,h,x,y);  //up
+}
+}
+
+int main()
+{
+int n,x1,y1;
+int x0=50,y0=150,x,y,h=10,r=2,d=3,l=4,u=1;
+cout<<"\nGive the value of n: ";
+cin>>n;
+x=x0;y=y0;
+moveto(x,y);
+int gm,gd=DETECT;
+initgraph(&gd,&gm,NULL);
+
+hilbert(r,d,l,u,n,h,x,y);
+delay(10000);
+closegraph();
+return 0;
+}
